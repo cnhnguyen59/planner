90c3432a9cbdec9e78d2fd4753595b04186b655f
Christine Nguyen <cnhnguyen59@gmail.com>
2021-12-02 00:27:36 -0500

    added styling to slots and added a js for time slot array

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,48 @@
 var currentDayEl = $('#currentDay')
-var timeSlots = [
-    {"time": "8:00 am"}
-]
+var currentHour = moment().startOf('hour').format('HH')
 
 currentDayEl.text(moment().format("MMM Do YY"))
 
-/* function slotTemplate(){
-    return `
-    <div class='slot'>
-        <div class='time'></div>
-        <textarea></textarea>
-        <button><i class="fa fa-save"></i></button>
+
+function slotTemplate(timeSlot){
+    var color = ''
+    if (timeSlot.hour == currentHour){
+        color = 'present'
+    } else if (timeSlot.hour < currentHour) {
+        color = 'past'
+    } else {
+        color = 'future'
+    }
+
+    return  `
+    <div class='row'>
+        <div class='col-lg-1 hour time-block'> ${timeSlot.time}</div>
+        <div class='col-lg-10 ${color}'>
+            <textarea >${timeSlot.text}</textarea>
+        </div>
+        <button class =' col-lg-1 saveBtn'><i class="fa fa-save"></i></button>
     </div>
     `
-} */
+}
+
 
 $('.container').html(
-    `
-    <div class='row'>
-        <div class='time'> 8:00 am</div>
-        <textarea></textarea>
-        <button class ='saveBtn'><i class="fa fa-save"></i></button>
-    </div>
-    `
+    `${timeSlots.map(slotTemplate).join('')}`
 )
 
+$('.row').on('click', 'button', (event)=>{
+    event.preventDefault()
+ /*    var targetEl = $(event.target)
+    var parentRow = targetEl.parent('div') 
+    var text = parentRow.children('textarea').val()
+    console.log(targetEl)
+    console.log(`text: ${text}`) */
+
+
+    var text = event.target.parentElement.parentElement.children[1].children[0].value
+
+    console.log(event.target)
+    console.log(event.target.parentElement.parentElement.children[1].children[0].value)
+
+
+})
---
+++ Develop/timeslots.js
@@ -0,0 +1,47 @@
+var timeSlots = [
+    { 
+        "time": "8:00 am",
+        "hour": "08",
+        "text": ""
+    },
+    { 
+        "time": "9:00 am",
+        "hour": "09",
+        "text": ""
+    },
+    { 
+        "time": "10:00 am",
+        "hour": "10",
+        "text": ""
+    },
+    { 
+        "time": "11:00 am",
+        "hour": "11",
+        "text": ""
+    },
+    { 
+        "time": "12:00 pm",
+        "hour": "12",
+        "text": ""
+    },
+    { 
+        "time": "1:00 pm",
+        "hour": "13",
+        "text": ""
+    },
+    { 
+        "time": "2:00 pm",
+        "hour": "14",
+        "text": ""
+    },
+    { 
+        "time": "3:00 pm",
+        "hour": "15",
+        "text": ""
+    },
+    { 
+        "time": "4:00 pm",
+        "hour": "16",
+        "text": ""
+    },  
+]
